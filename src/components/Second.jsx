7594f60590eee115bc5ff60d58f6ca7994fbf543
Lamia Fatiha <f6lamia@gmail.com>
2024-07-03 22:44:18 +0600

    starting 3rd

--- src/components/Second.jsx
+++ src/components/Second.jsx
@@ -1,9 +1,20 @@
+import shop from '../assets/image/shop.jpg'
 
 const Second = () => {
   return (
-    <div className='w-full h-[780px] bg-black'>
-      hello
+    <div className='w-full h-[580px] bg-center bg-cover' 
+     style={{ 
+        backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.3), 
+                          rgba(0, 0, 0, 0.3)), url(${shop})`
+      }}>
+
+      <div className='flex justify-center items-center flex-col pt-40'>
+      <h1 className='text-5xl text-white font-bold'>Women's Collection </h1>
+      <button className='w-40 h-12 bg-black text-white text-2xl mt-4'>Shop Now</button>
+      </div>
     </div>
+    
+    
   )
 }
 
