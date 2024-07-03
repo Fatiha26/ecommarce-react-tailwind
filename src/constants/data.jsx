f3af73a8f11b27553fef7fff609ee0ae16ccefe6
Lamia Fatiha <f6lamia@gmail.com>
2024-07-03 20:53:44 +0600

    starting slider

--- src/constants/data.jsx
+++ src/constants/data.jsx
@@ -13,3 +13,24 @@ export const data = [
         text: "KIDS",
     }
 ]
+
+import pic1 from '../assets/image/c1.jpeg'
+import pic2 from '../assets/image/c2.jpg'
+import pic3 from '../assets/image/c3.jpg'
+import pic4 from '../assets/image/c4.jpeg'
+
+
+export const slider = [
+    {
+        src : pic1,
+    },
+    {
+        src : pic2,
+    },
+    {
+        src : pic3,
+    },
+    {
+        src : pic4,
+    },
+]
