ba3cf48c66b5592c226e06f34ba76b1364225e98
Lamia Fatiha <f6lamia@gmail.com>
2024-07-03 20:21:59 +0600

    content added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import logo from "../assets/image/logo1.png";
+import { data } from '../constants/data';
+import { CiSearch, CiUser, CiHeart } from "react-icons/ci";
+import { IoBagOutline } from "react-icons/io5";
 
 const Navbar = () => {
   return (
     <div>
-      
+      <div>
+      <div>
+      <img src={logo} alt="" /><span>Elegance Echo</span>
+      </div>
+        <div>
+        {data.map((item,index)=>(
+            <div key={index}>
+               <a href={item.link}>{item.text}</a>
+            </div>
+        ))}
+        </div>
+      </div>
+      <div>
+         <CiSearch />
+         <CiUser />
+         <CiHeart />
+         <IoBagOutline /> 
+      </div>
     </div>
   )
 }
