af3cd1a184651fe936e3223d2030f2fe24b97e00
Lamia Fatiha <f6lamia@gmail.com>
2024-07-03 22:44:57 +0600

    positioning

--- src/components/Second.jsx
+++ src/components/Second.jsx
@@ -8,7 +8,7 @@ const Second = () => {
                           rgba(0, 0, 0, 0.3)), url(${shop})`
       }}>
 
-      <div className='flex justify-center items-center flex-col pt-40'>
+      <div className='flex justify-center items-center flex-col pt-44'>
       <h1 className='text-5xl text-white font-bold'>Women's Collection </h1>
       <button className='w-40 h-12 bg-black text-white text-2xl mt-4'>Shop Now</button>
       </div>
