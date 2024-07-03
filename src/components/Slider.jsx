13baadd00f18f18348bf5029d898115b23f54c7d
Lamia Fatiha <f6lamia@gmail.com>
2024-07-03 22:47:50 +0600

    hero banner

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -34,7 +34,8 @@ const Slider = () => {
       >
         <Navbar />
         <div className='flex justify-center items-center flex-col pt-44'>
-      <h1 className='text-5xl text-white font-bold'>Women's Collection </h1>
+      <h1 className='text-5xl text-white font-bold'>
+      Welcome to<span className="text-5xl md:text-md font-bold text-pink-500">Elegance Echo</span></h1>
       <button className='w-40 h-12 bg-black text-white text-2xl mt-4'>Shop Now</button>
       </div>
       </div>
