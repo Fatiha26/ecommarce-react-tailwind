45fdfa9dde9c8747e87220a499cad8a89042fb57
Lamia Fatiha <f6lamia@gmail.com>
2024-07-04 20:42:46 +0600

    whats new section done

--- src/components/New.jsx
+++ src/components/New.jsx
@@ -6,11 +6,13 @@ import 'swiper/css/free-mode';
 import 'swiper/css/navigation';
 import { FreeMode, Pagination, Navigation } from 'swiper/modules';
 import { newItem } from '../constants/data';
+import { IoIosAddCircleOutline } from "react-icons/io";
 
 const New = () => {
   return (
     <>
-      <div>
+      <div className='my-12'>
+      <h1 className='text-center text-6xl font-bold'>What&apos;s new</h1>
       <div className='flex items-center justify-center flex-col h-screen relative'>
         <Swiper
           breakpoints={{
@@ -39,17 +41,17 @@ const New = () => {
               <div className='flex flex-col gap-6 group relative shadow-lg to-white rounded-xl px-6 py-8 h-[250px] w-[215px] lg:h-[400px] lg:w-[350px] cursor-pointer'>
                 <div
                   className='absolute inset-0 bg-cover bg-center'
-                  style={{ backgroundImage: `url(${item.src})` }}
-                ></div>
+                  style={{ backgroundImage: `url(${item.src})` }}>
+                    <div className='absolute bottom-3 left-3'>
+                      <IoIosAddCircleOutline className='text-5xl text-pink-500' />
+                    </div>
+                  </div>
               </div>
             </SwiperSlide>
           ))}
         </Swiper>
-        <div className='swiper-button-prev absolute z-10 top-50 cursor-pointer'>
-
-        </div>
-        <div className='swiper-button-next absolute z-10 top-50 cursor-pointer'>
-        </div>
+        <div className='swiper-button-prev absolute z-10 top-50 cursor-pointer'></div>
+        <div className='swiper-button-next absolute z-10 top-50 cursor-pointer'></div>
       </div>
       </div>
     </>
