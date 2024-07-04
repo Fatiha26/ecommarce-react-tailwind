18e2791f929f04b52d62bdf32e129a765d05c78e
Lamia Fatiha <f6lamia@gmail.com>
2024-07-04 22:22:53 +0600

    news img added

--- src/components/Stories.jsx
+++ src/components/Stories.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import pic1 from '../assets/image/k2.jpg'
+import pic2 from '../assets/image/s1.jpg'
+import pic3 from '../assets/image/c1.jpeg'
 
 const Stories = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+       <div className='my-16 text-center'>
+        <h1 className='text-center text-4xl font-bold'>Stories</h1>
+        <div className='grid grid-cols-[60%_40%] gap-8 px-12 py-12'>
+            <img src={pic1} alt="" />
+            <div>
+            <img src={pic2} alt="" />
+            <img src={pic3} alt="" />
+            </div>
+        </div>
+       </div>
+    </>
   )
 }
 
