b6de9bfd8fe7d64ddb49671accfdd7e12f19a010
Lamia Fatiha <f6lamia@gmail.com>
2024-07-04 20:04:41 +0600

    content added

--- src/components/New.jsx
+++ src/components/New.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css'
+import 'swiper/css/pagination'
+import 'swiper/css/free-mode'
+import { FreeMode, Pagination } from 'swiper/modules'
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
+import { newItem } from '../constants/data'
+
 
 const New = () => {
   return (
     <>
-        
+    <div className='flex items-center justify-center flex-col h-screen'>
+     <Swiper
+      breakpoints={{
+        340:{
+            slidesPerView:2,
+            spaceBetween: 15
+        },
+        700:{
+            slidesPerView:3,
+            spaceBetween: 15
+        },
+      }}
+      freeMode={true}
+      pagination={{
+        clickable:true
+      }}
+      modules={[FreeMode,Pagination]}
+      className='max-w-[90%] lg:max-w-[80%]'
+      >
+
+     </Swiper>
+
+    </div>
+
     </>
   )
 }
--- src/constants/data.jsx
+++ src/constants/data.jsx
@@ -1,3 +1,12 @@
+import pic1 from '../assets/image/k1.jpg'
+import pic2 from '../assets/image/k2.jpg'
+import pic3 from '../assets/image/c3.jpg'
+import pic4 from '../assets/image/k1.jpg'
+import product1 from '../assets/image/p1.jpg'
+import product2 from '../assets/image/p2.jpg'
+import product3 from '../assets/image/p3.jpg'
+import product4 from '../assets/image/p4.jpg'
+
 
 export const data = [
     {
@@ -14,10 +23,7 @@ export const data = [
     }
 ]
 
-import pic1 from '../assets/image/k1.jpg'
-import pic2 from '../assets/image/k2.jpg'
-import pic3 from '../assets/image/c3.jpg'
-import pic4 from '../assets/image/k1.jpg'
+
 
 
 export const sliders = [
@@ -41,4 +47,12 @@ export const downNav = [
     {feature : "KIDS",},
     {feature : "JEWELLERY",},
     {feature : "SKIN CARE",},
+]
+
+export const newItem = [
+    {src:product1},
+    {src:product2},
+    {src:product3},
+    {src:product4},
+
 ]
