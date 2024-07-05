e1895fc278e5827190e2bc900cb5f13254138881
Lamia Fatiha <f6lamia@gmail.com>
2024-07-05 11:34:31 +0600

    started stay tuned section

---
+++ src/components/StayTuned.jsx
@@ -0,0 +1,30 @@
+import { FaRegEnvelopeOpen } from "react-icons/fa6";
+const StayTuned = () => {
+  return (
+    <>
+     <div>
+        <div>
+          <div>
+           <FaRegEnvelopeOpen />
+           <h4>STAY TUNED</h4>
+          </div>
+          <p>Don’t miss the opportunity to get daily updates on all that’s new at Elegance Echo</p>
+        </div>
+        <div>
+          <div>
+            <input type="text" />
+            <input type="text" />
+            <button></button>
+          </div>
+          <div>
+            <input type="radio" name="" id="" />
+            <input type="radio" name="" id="" />
+          </div>
+        </div>
+     </div>
+      
+    </>
+  )
+}
+
+export default StayTuned
