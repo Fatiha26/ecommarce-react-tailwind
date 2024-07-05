f3d38e6574fa8cd9cc8c51b07de81074af8c1f1e
Lamia Fatiha <f6lamia@gmail.com>
2024-07-05 12:51:09 +0600

    content added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,25 @@
-
+import apple from '../assets/image/apple.png'
+import google from '../assets/image/google.png'
+import { FaFacebook, FaInstagram, FaLinkedinIn } from "react-icons/fa";
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <footer>
+         <div>
+            <p>&copy; Lamia Fatiha 2024. All rights reserved.</p>
+         </div>
+         <div>
+          <FaFacebook />
+          <FaInstagram />
+          <FaLinkedinIn  />
+         </div>
+         <div>
+           <img src={google} alt="" />
+           <img src={apple} alt="" />
+         </div>
+
+      </footer>
+    </>
   )
 }
 
