4440e7602101e9efe021fa9c05211cd3f64ad84a
Lamia Fatiha <f6lamia@gmail.com>
2024-07-05 21:42:26 +0600

    responsive footer done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,18 +4,18 @@ import { FaFacebook, FaInstagram, FaLinkedinIn } from "react-icons/fa";
 const Footer = () => {
   return (
     <>
-      <footer className='flex justify-between px-12 items-center bg-black py-4'>
+      <footer className='md:flex justify-between px-7 md:px-12 items-center bg-black py-4'>
          <div>
             <p className='text-lg text-white'>&copy; Lamia Fatiha 2024. All rights reserved.</p>
          </div>
-         <div className='flex gap-4 text-2xl text-white'>
+         <div className='flex md:gap-4 gap-12 md:ml-0 ml-14 text-2xl text-white my-2 md:my-0'>
           <FaFacebook />
           <FaInstagram />
           <FaLinkedinIn  />
          </div>
-         <div className='w-20 h-8 flex gap-4 mr-24'>
+         <div className='w-20 h-8 flex gap-4 md:mr-24 ml-12 md:ml-0'>
            <img src={google} alt="" />
-           <img src={apple} alt="" />
+           <img src={apple} alt="" className='h-6 mt-1'/>
          </div>
 
       </footer>
