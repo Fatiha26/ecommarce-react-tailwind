86797b4c585a2abdba29bd4ced48484f0759fdc8
Lamia Fatiha <f6lamia@gmail.com>
2024-07-05 22:00:49 +0600

    responsive category section  done

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -5,12 +5,12 @@ import p5 from '../assets/image/p5.jpg'
 const Category = () => {
   return (
     <div>
-      <div className='grid grid-cols-2 w-full h-screen'>
+      <div className='grid md:grid-cols-2 w-full md:h-screen'>
          <div style={{ 
                backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.5), 
                           rgba(0, 0, 0, 0.5)), url(${c1})`
-                  }} className='w-full bg-no-repeat bg-cover bg-center h-screen'>
-               <div className='flex justify-center items-center flex-col pt-64'>
+                  }} className='w-full h-[400px] md:h-screen bg-no-repeat bg-cover bg-center'>
+               <div className='flex justify-center items-center flex-col md:pt-64 pt-40'>
                <h1 className='text-3xl text-white font-bold'>Silver Collection </h1>
                <p className='text-xl text-white'>Explore Jewellery</p>
                <button className='w-32 h-12 bg-black text-white text-xl mt-4 border-b-2 transition delay-75 border-transparent hover:border-white'>Shop Now</button>
@@ -19,8 +19,8 @@ const Category = () => {
          <div style={{ 
                backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.3), 
                           rgba(0, 0, 0, 0.3)), url(${c2})`
-                  }} className='w-full  h-screen bg-no-repeat bg-center bg-cover'>
-             <div className='flex justify-center items-center flex-col pt-64'>
+                  }} className='w-full h-[400px] md:h-screen bg-no-repeat bg-center bg-cover'>
+             <div className='flex justify-center items-center flex-col md:pt-64 pt-40'>
                <h1 className='text-3xl text-white font-bold'>Diamond Collection </h1>
                <p className='text-xl text-white'>Explore Jewellery</p>
                <button className='w-32 h-12 bg-black text-white text-xl mt-4 border-b-2 transition delay-75 border-transparent hover:border-white'>Shop Now</button>
@@ -32,8 +32,8 @@ const Category = () => {
       <div style={{ 
                backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.3), 
                           rgba(0, 0, 0, 0.3)), url(${p5})`
-                  }} className='w-full  h-screen bg-no-repeat bg-top bg-cover'>
-             <div className='flex justify-center items-center flex-col pt-64'>
+                  }} className='w-full  h-[400px] md:h-screen bg-no-repeat bg-top bg-cover'>
+             <div className='flex justify-center items-center flex-col md:pt-64 pt-40'>
                <h1 className='text-3xl text-white font-bold'>Girl&apos;s Collection </h1>
                <p className='text-xl text-white'>Explore Collection</p>
                <button className='w-32 h-12 bg-black text-white text-xl mt-4 border-b-2 transition delay-75 border-transparent hover:border-white'>Shop Now</button>
