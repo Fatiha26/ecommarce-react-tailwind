b251cc5c36d4a6e2c9a4dc6a592df71def6dcf96
Lamia Fatiha <f6lamia@gmail.com>
2024-07-05 21:45:21 +0600

    responsive about done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,14 +3,14 @@ import { about } from '../constants/data'
 const About = () => {
   return (
     <>
-      <div className='grid grid-cols-4'>
+      <div className='grid md:grid-cols-4 grid-cols-2'>
       {
         about.map((item,index)=>(
             <div key={index} style={{ 
                backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.5), 
                           rgba(0, 0, 0, 0.5)), url(${item.src})`
-                  }} className='w-full bg-no-repeat bg-cover bg-center  h-[400px] relative'>
-               <div className='flex justify-center items-center flex-col pt-44'>
+                  }} className='w-full bg-no-repeat bg-cover bg-center h-[250px] md:h-[400px] relative'>
+               <div className='flex justify-center items-center flex-col pt-28 md:pt-44'>
                <h1 className='text-xl tracking-wider text-white font-bold'>{item.text}</h1>
             </div>
                 {/* <div className='absolute text-center py-20 top-0  left-0 text-white 
